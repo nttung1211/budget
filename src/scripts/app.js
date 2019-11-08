6c531ae7b6c0e3de0f33597245fa0537395c46a2
Clark <nttung1211@gmail.com>
2019-11-08 17:42:03 +0700

    Finish setup environment.

---
+++ src/scripts/app.js
@@ -0,0 +1,5 @@
+import '../css/app.scss';
+import { Person } from './modules/Person.js';
+
+let clark = new Person(`Clark`);
+console.log(clark.name);
---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+const path = require('path'),
+    webpack = require(`webpack`),
+    MiniCssExtractPlugin = require('mini-css-extract-plugin'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    { CleanWebpackPlugin } = require('clean-webpack-plugin'),
+    imageminGifsicle = require('imagemin-gifsicle'),
+    imageminMozjpeg = require('imagemin-mozjpeg'),
+    imageminPngquant = require('imagemin-pngquant'),
+    imageminSvgo = require('imagemin-svgo');
+
+module.exports = {
+    mode: 'development',
+    watchOptions: {
+        ignored: /node_modules/,
+        aggregateTimeout: 500
+    },
+    entry: {
+        App: './src/scripts/app.js'
+    },
+    output: {
+        filename: './[name].bundle.js',
+        path: path.resolve(__dirname, './docs'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                ['@babel/preset-env', { useBuiltIns: 'usage', corejs: 3 }]
+                            ]
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: './images',
+                            publicPath: './images'
+                        }
+                    },
+                    {
+                        loader: 'img-loader',
+                        options: {
+                            plugins: [
+                                imageminGifsicle(),
+                                imageminMozjpeg(),
+                                imageminPngquant(),
+                                imageminSvgo()
+                            ]
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
+        new MiniCssExtractPlugin({
+            filename: './[name].bundle.css'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new CleanWebpackPlugin(),
+    ]
+};
